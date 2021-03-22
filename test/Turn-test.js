1b7f8df510cd71ed8ec48ef2dc85399542f99caa
Your Name <stoner.michann@gmail.com>
2021-03-22 17:10:03 -0600

    Create it block for instance of Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,4 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it.skip('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceOf(Turn);
+  });
