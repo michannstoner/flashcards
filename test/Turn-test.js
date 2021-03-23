fe4a9a77219dca01d5fa8b6b521bcf0cb040b0a6
Your Name <stoner.michann@gmail.com>
2021-03-22 19:42:48 -0600

    Write implementation code for Turn.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,24 +6,24 @@ const Card = require('../src/Card');
 
 describe('Turn', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceOf(Turn);
   });
 
-  it.skip('should have a guess and a current card in play', function() {
+  it('should have a guess and a current card in play', function() {
     const card = new Card(1, 'What is the name of a monster that is said to roam the PNW', ['Monster', 'Sasquatch', 'Gorilla'], 'Sasquatch');
     const turn = new Turn('Sasquatch', card);
     expect(turn.guess).to.deep.equal('Sasquatch');
     expect(turn.currentCard).to.deep.equal(card);
     });
 
-  it.skip('should show the player their guess', function() {
+  it('should show the player their guess', function() {
     const card = new Card(1, 'What is the name of a monster that is said to roam the PNW', ['Monster', 'Sasquatch', 'Gorilla'], 'Sasquatch');
     const turn = new Turn('Sasquatch', card);
     
@@ -31,15 +31,16 @@ describe('Turn', function() {
     expect(guess1).to.deep.equal('Sasquatch');
   });
 
-  it.skip('should show the player the current card', function() {
+  it('should show the player the current card', function() {
     const card = new Card(1, 'What is the name of a monster that is said to roam the PNW', ['Monster', 'Sasquatch', 'Gorilla'], 'Sasquatch');
     const turn = new Turn('Sasquatch', card);
+    
 
     const card1 = turn.returnCard();
-    expect(card1).to.deep.equal(1, 'What is the name of a monster that is said to roam the PNW', ['Monster', 'Sasquatch', 'Gorilla'], 'Sasquatch');
+    expect(card1).to.deep.equal(card);
   });
 
-  it.skip('should evaluate if the guess matches the correct answer', function() {
+  it('should evaluate if the guess matches the correct answer', function() {
     const card = new Card(1, 'What is the name of a monster that is said to roam the PNW', ['Monster', 'Sasquatch', 'Gorilla'], 'Sasquatch');
     const turn = new Turn('Sasquatch', card);
     
@@ -48,15 +49,17 @@ describe('Turn', function() {
     expect(turn.isCorrect).to.deep.equal(true);
   });
 
-  it.skip('should give feedback if the guess is correct', function() {
+  it('should give feedback if the guess is correct', function() {
+    const card = new Card(1, 'What is the name of a monster that is said to roam the PNW', ['Monster', 'Sasquatch', 'Gorilla'], 'Sasquatch');
     const turn = new Turn('Sasquatch', card);
     
     const feedback1 = turn.giveFeedback();
     expect(feedback1).to.equal('Correct!');
   });
 
-  it.skip('should give feedback if the guess is incorrect', function() {
-    const turn = new Turn('Sasquatch', card);
+  it('should give feedback if the guess is incorrect', function() {
+    const card = new Card(1, 'What is the name of a monster that is said to roam the PNW', ['Monster', 'Sasquatch', 'Gorilla'], 'Sasquatch');
+    const turn = new Turn('Monster', card);
     
     const feedback2 = turn.giveFeedback();
     expect(feedback2).to.equal('Incorrect!');
