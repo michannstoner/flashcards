b01fe9a25053c8794e7a245ee11c888038a18458
Your Name <stoner.michann@gmail.com>
2021-03-25 17:12:55 -0600

    Complete start method for game class

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,5 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
-//prototypeData becomes a deck of cards / deck class / round class
 const util = require('./util');
 const Round = require('../src/Round');
 const Card = require('../src/Card');
@@ -9,20 +8,25 @@ const Turn = require('../src/Turn');
 
 class Game {
   constructor() {
-
+    this.currentRound;
   }
   start() {
     const cardData = prototypeQuestions.map((prototype) => {
       prototype = new Card(prototype.id, prototype.question, prototype.answers, prototype.correctAnswer);
       return prototype;
-    })
-  }
+    )};
 
+    let deck = new Deck(cardData);
+    this.currentRound = new Round(deck);
+
+    this.printMessage(this.currentRound.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+    this.currentRound.endRound;
+  }
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
-
   printQuestion(round) {
       util.main(round);
   }
